71ee8b8e9b4fc225e8547affe6b7a63cc8511415
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-01-16 20:09:57 +0200

    unit test added for rejection

--- test/index.js
+++ test/index.js
@@ -1,6 +1,9 @@
 'use strict'
-const expect = require('chai').expect
+const chai = require('chai')
+const expect = chai.expect
 const uva = require('../')
+
+chai.use(require('chai-as-promised'))
 
 describe('uva-amqp', function() {
   it('should successfully call a remote function', function(done) {
@@ -40,6 +43,20 @@ describe('uva-amqp', function() {
       expect(err).to.be.instanceOf(Error)
       done()
     })
+  })
+
+  it('should reject if no response has been recieved in time', function(done) {
+    let client = new uva.Client({
+      channel: 'foo',
+      url: 'amqp://guest:guest@localhost:5672',
+      ttl: 100,
+    })
+
+    client.register(['bar'])
+
+    let res = client.methods.bar()
+
+    expect(res).to.be.rejectedWith(Error).notify(done)
   })
 
   it('should return promise if last argunent not a callback', function() {
